78564e544129e12b0f58cd36b46d10eefd6227ee
Landon Gray <landon.gray@testdouble.com>
2024-03-28 20:44:42 -0400

    Add a create action

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -5,4 +5,14 @@ class WeatherForecastsController < ApplicationController
   def new
     @weather_forecast = WeatherForecast.new
   end
+
+  def create
+    @weather_forecasts = WeatherForecast.new(weather_forecast_params)
+    redirect_to @weather_forecasts
+  end
+
+  private
+  def weather_forecast_params
+    params.require(:weather_forecast).permit(:address)
+  end
 end
