f4c9712494350d50153908da9fa33362016befbf
Landon Gray <landon.gray@testdouble.com>
2024-03-31 18:13:01 -0400

    Update tests to accept address parts

--- spec/system/weather_forecast_spec.rb
+++ spec/system/weather_forecast_spec.rb
@@ -3,8 +3,12 @@ require "rails_helper"
 describe "Address submission", type: :feature do
   it "User submits an address" do
     visit "/"
+
     within("form") do
-      fill_in "Address", with: "1981 Broadway, New York, NY 10023"
+      fill_in "Street", with: "1981 Broadway"
+      fill_in "City", with: "New York"
+      fill_in "State", with: "NY"
+      fill_in "Zip", with: "10023"
     end
 
     click_button "Create Weather forecast"
