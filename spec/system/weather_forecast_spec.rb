57ceb754dcd95d373a62ba8e809a2c4fc41d114e
Landon Gray <landon.gray@testdouble.com>
2024-04-02 13:35:39 -0400

    Add zip code validation check

--- spec/system/weather_forecast_spec.rb
+++ spec/system/weather_forecast_spec.rb
@@ -17,6 +17,23 @@ describe "Address submission", type: :feature do
     expect(page).to have_button("Back To Address Submission")
   end
 
+  it "User submits an address with invalid zip code" do
+    visit "/"
+
+    within("form") do
+      fill_in "Street", with: "1981 Broadway"
+      fill_in "City", with: "New York"
+      fill_in "State", with: "NY"
+      fill_in "Zip", with: "1234"
+    end
+
+    click_button "Create Weather forecast"
+
+    expect(page).not_to have_content "Zip can't be blank"
+    expect(page).to have_content "Zip should be in the form 12345"
+    expect(page).to have_button("Create Weather forecast")
+  end
+
   it "User submits an address without zip code" do
     visit "/"
 
@@ -29,6 +46,7 @@ describe "Address submission", type: :feature do
     click_button "Create Weather forecast"
 
     expect(page).to have_content "Zip can't be blank"
+    expect(page).to have_content "Zip should be in the form 12345"
     expect(page).to have_button("Create Weather forecast")
   end
 end
