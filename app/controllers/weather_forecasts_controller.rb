e5e08f2c8c15c8f751fda9236bae7b586801f155
Landon Gray <landon.gray@testdouble.com>
2024-04-02 21:48:43 -0400

    Refactor weather controller

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -8,21 +8,10 @@ class WeatherForecastsController < ApplicationController
 
   def create
     @weather_forecast = WeatherForecast.new(weather_forecast_params)
-    if @weather_forecast.invalid? && @weather_forecast.errors.any?
+    if @weather_forecast.invalid?
       render :new
     else
-      accu_service = AccuWeatherV1Api.new
-      result = accu_service.weather_for_zipcode(weather_forecast_params[:zip])
-      location = get_us_location(result)
-      one_day_forecast = accu_service.get_1_day_forecast(location.dig("Key"))
-      temperature = one_day_forecast.dig("DailyForecasts", 0, "Temperature")
-
-      weather_forecast = {
-        location: location["LocalizedName"],
-        high: temperature.dig("Maximum", "Value").to_s + temperature.dig("Maximum", "Unit"),
-        low: temperature.dig("Minimum", "Value").to_s + temperature.dig("Minimum", "Unit")
-      }
-
+      weather_forecast = fetch_weather_forecast(weather_forecast_params[:zip])
       render :show, locals: {weather_forecast: weather_forecast}
     end
   end
@@ -31,6 +20,34 @@ class WeatherForecastsController < ApplicationController
   end
 
   private
+
+  def fetch_weather_forecast(zip)
+    location = get_location(zip)
+    localized_name = location.dig("LocalizedName")
+
+    one_day_temp = get_one_day_forecast(location.dig("Key"))
+    {
+      location: localized_name,
+      high: format_temperature(one_day_temp["Maximum"]),
+      min: format_temperature(one_day_temp["Minimum"])
+    }
+  end
+
+  def format_temperature(temperature)
+    "#{temperature["Value"]}#{temperature["Unit"]}"
+  end
+
+  def get_location(zip)
+    accu_service = AccuWeatherV1Api.new
+    result = accu_service.weather_for_zipcode(zip)
+    get_us_location(result)
+  end
+
+  def get_one_day_forecast(key)
+    accu_service = AccuWeatherV1Api.new
+    one_day_forecast = accu_service.get_1_day_forecast(key)
+    one_day_forecast.dig("DailyForecasts", 0, "Temperature")
+  end
 
   def get_us_location(parsed_json)
     parsed_json.find { |location| location.dig("Country", "ID") == "US" }
