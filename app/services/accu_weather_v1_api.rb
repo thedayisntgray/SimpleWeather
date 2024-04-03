1a89c935c6140a80ae19d3ac81c8bf16547a3823
Landon Gray <landon.gray@testdouble.com>
2024-04-03 17:34:30 -0400

    Dry up duplication in response handling

--- app/services/accu_weather_v1_api.rb
+++ app/services/accu_weather_v1_api.rb
@@ -11,28 +11,22 @@ class AccuWeatherV1Api
   def get_1_day_forecast(location_key)
     uri = URI("#{API_ENDPOINT}/forecasts/v1/daily/1day/#{location_key}")
 
-    params = {apikey: @api_key}
-    uri.query = URI.encode_www_form(params)
+    uri.query = URI.encode_www_form({apikey: @api_key})
     response = Net::HTTP.get_response(uri)
 
-    case response
-    when Net::HTTPSuccess
-      JSON.parse(response.body)
-    when Net::HTTPServiceUnavailable
-      parsed_body = JSON.parse(response.body)
-      {error: parsed_body["Message"]}
-    else
-      {error: "An error occured, please contact the administrator at test@test.com"}
-    end
+    handle_response(response)
   end
 
   def weather_for_zipcode(zip_code)
     uri = URI("#{API_ENDPOINT}/locations/v1/search")
 
-    params = {q: zip_code, apikey: @api_key}
-    uri.query = URI.encode_www_form(params)
+    uri.query = URI.encode_www_form({q: zip_code, apikey: @api_key})
     response = Net::HTTP.get_response(uri)
 
+    handle_response(response)
+  end
+
+  def handle_response(response)
     case response
     when Net::HTTPSuccess
       JSON.parse(response.body)
